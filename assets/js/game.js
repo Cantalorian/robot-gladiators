3c57288506a85ef6bc1d4acc22fb14d97c2b4075
Chewie <chewie1985@gmail.com>
2021-03-21 20:07:34 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,22 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyName, enemyAttack, enemyHealth);
-var playerMoney = 10;
+// Game states
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" = Player robot's health is zero or less
 
-var fight = function () {
+var fight = function (enemyName) {
   // Alert players they are starting the round
   window.alert("Welcome to Robot Gladiators!");
-
+  
   var promptFight = window.prompt(
     "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
   );
@@ -74,18 +76,17 @@ var fight = function () {
     }
     // if no (false), ask question again by running fight() again
     else {
-    fight();
-    //window.alert("You need to choose a valid option. Try again!");
+      fight();
+      //window.alert("You need to choose a valid option. Try again!");
     }
-  };
+  }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+//fight(enemyRobot);
 
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
 console.log("Our robot's name is " + playerName);
 
 function fight() {
